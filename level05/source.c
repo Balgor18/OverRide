a595cc0ff78663cf7bab221214ecaafc07618f62
Florian Catinaud <catinaud.florian@gmail.com>
2024-12-11 21:52:14 +0100

    Source file and flag of level5

---
+++ level05/source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    char buf[100];
+
+    fgets(buf, 100, 0);
+
+    for (i = 0; i < strlen(buf); i++)
+        if (buf[i] >= 'A' && buf[i] <= 'Z')
+            buf[i] = buf[i] ^ 0x20;
+
+    printf(buf);
+    exit(0);
+    return 1;
+}
