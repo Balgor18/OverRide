112c8a694b8df1cfd351119e7733d6a2c9a574ff
nils-mathieu <nils.mathieu.contact@gmail.com>
2024-12-12 15:20:29 +0100

    added level06 source

--- level06/source.zig
+++ level06/source.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const libc = @cImport({
+    @cInclude("stdlib.h");
+    @cInclude("unistd.h");
+});
 
 fn readLine(reader: std.fs.File, buf: []u8) ![]const u8 {
     const len = try reader.read(buf);
@@ -6,6 +10,16 @@ fn readLine(reader: std.fs.File, buf: []u8) ![]const u8 {
         return error.InputTooLarge;
     }
     return std.mem.trim(u8, buf[0..len], "\r\n ");
+}
+
+fn readNumber(file: std.fs.File) !u32 {
+    var buf: [64]u8 = undefined;
+    const line = try readLine(file, &buf);
+    if (std.mem.startsWith(u8, line, "0x")) {
+        return std.fmt.parseUnsigned(u32, line[2..], 16);
+    } else {
+        return std.fmt.parseUnsigned(u32, line, 10);
+    }
 }
 
 fn crack_code(input: []const u8) error{InvalidInput}!u32 {
@@ -25,6 +39,8 @@ fn crack_code(input: []const u8) error{InvalidInput}!u32 {
 }
 
 pub fn main() !void {
+    _ = libc.alarm(60); // lol
+
     const stdout = std.io.getStdOut();
     const stdin = std.io.getStdIn();
 
@@ -32,6 +48,12 @@ pub fn main() !void {
     var buf: [32]u8 = undefined;
     const input = try readLine(stdin, &buf);
 
+    try stdout.writeAll("Enter serial: ");
+    const serial = try readNumber(stdin);
+
     const result = try crack_code(input);
-    try stdout.writer().print("Result: {}\n", .{result});
+
+    if (serial == result) {
+        _ = libc.system("/bin/sh");
+    }
 }
