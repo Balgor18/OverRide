07f96a83eb980657dd5901be6ccfb21054a011f2
Florian Catinaud <catinaud.florian@gmail.com>
2024-12-16 01:21:47 +0100

    Push the source code of the level7 8 and 9.

--- level08/source.c
+++ level08/source.c
@@ -1,61 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-#define BACKUP_DIR "./backups/"
-#define LOG_FILE "./backups/.log"
+unsigned int	log_wrapper(FILE	*logfile, const char	*str, const char	*filename)
+{
+	char	dest[264];
 
-void log_wrapper(FILE *logf, char *msg, char *filename) {
-	char log_buf[255];
-	strcpy(log_buf, msg);
-	snprintf(log_buf+strlen(log_buf), 255-strlen(log_buf)-1/*NULL*/, filename);
-	log_buf[strcspn(log_buf, "\n")] = '\0';
-	fprintf(logf, "LOG: %s\n", log_buf);
+	strcpy(dest, str);
+	snprintf(&dest[strlen(dest)], 254 - strlen(dest), filename);
+	dest[strcspn(dest, "\n")] = 0;
+	fprintf(logfile, "LOG: %s\n", dest);
+	return 0;
 }
 
-int	main(int argc, char *argv[]) {
-	char ch = EOF;
-	char dest_buf[100];
-	FILE *source, *logf;
-	int target = -1;
+int	main(int	argc, const char	**argv, const char	**envp)
+{
+	FILE	*log;
+	FILE	*file;
+	int		fd;
+	char	buffer;
+	char	dest[104];
 
-	if (argc != 2) {
-		printf("Usage: %s filename\n", argv[0]);
+	buffer = -1;
+	if ( argc != 2 )
+		printf("Usage: %s filename\n", *argv);
+	log = fopen("./backups/.log", "w");
+	if ( !log )
+	{
+		printf("ERROR: Failed to open %s\n", "./backups/.log");
+		exit(1);
 	}
-
-	logf = fopen(LOG_FILE, "w");
-	if (logf == NULL) {
-		printf("ERROR: Failed to open %s\n", LOG_FILE);
-		exit(EXIT_FAILURE);
-	}
-
-	log_wrapper(logf, "Starting back up: ", argv[1]);
-
-	source = fopen(argv[1], "r");
-	if (source == NULL) {
+	log_wrapper(log, "Starting back up: ", argv[1]);
+	file = fopen(argv[1], "r");
+	if ( !file )
+	{
 		printf("ERROR: Failed to open %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
-
-	strcpy(dest_buf, BACKUP_DIR);
-	strncat(dest_buf, argv[1], 100-strlen(dest_buf)-1/*NULL*/);
-	target = open(dest_buf, O_CREAT | O_EXCL | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-	if (target < 0) {
-		printf("ERROR: Failed to open %s%s\n", BACKUP_DIR, argv[1]);
-		exit(EXIT_FAILURE);
+	strcpy(dest, "./backups/");
+	strncat(dest, argv[1], 99 - strlen(dest));
+	fd = open(dest,  O_RDONLY | O_WRONLY | O_EXCL | O_CREAT );
+	if ( fd < 0 )
+	{
+		printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
+		exit(1);
 	}
+	while ( 1 )
+	{
+		buffer = fgetc(file);
+		if ( buffer == -1 )
+		break;
+		write(fd, &buffer, 1uLL);
+	}
+	log_wrapper(log, "Finished back up ", argv[1]);
+	fclose(file);
+	close(fd);
 
-	while( ( ch = fgetc(source) ) != EOF)
-		write(target, &ch, 1);
-
-	log_wrapper(logf, "Finished back up ", argv[1]);
-
-	fclose(source);
-	close(target);
-
-	return EXIT_SUCCESS;
+	return 0;
 }
